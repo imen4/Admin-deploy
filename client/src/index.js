77d8c159ca50c323339a1920f94cd2cf3c2fa57b
imen <imenjlassi02@gmail.com>
2023-02-26 12:16:19 +0100

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,11 @@ import ReactDOM from 'react-dom/client';
 import { store } from './JS/store/store';
 import './index.css';
 import App from './App';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
+import {disableReactDevTools} from "@fvilers/disable-react-devtools";
+
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
